8e0592a1bd6d034cc2bf10c213966b9223edc25a
kusumavishu <vishukusuma2003@gmail.com>
2025-01-26 18:13:06 +0530

    Home-carousel

--- lib/data.ts
+++ lib/data.ts
@@ -32,6 +32,36 @@ const data = {
             name: "Help",
             href: "/page/help",
         },
+    ],
+    carousels: [
+        {
+            title: "Most Popular Shoes For Sales",
+            buttonCaption: "Shop Now",
+            image: "/images/banner1.jpg",
+            url: "/search?category=Shoes",
+            isPublished: true
+        },
+        {
+            title: "Best Deals on Wrist Watches",
+            buttonCaption: "Shop Now",
+            image: "/images/banner3.jpg",
+            url: "/search?category=Wrist Watches",
+            isPublished: true
+        },
+        {
+            title: "Best Combo Shirts",
+            buttonCaption: "Shop Now",
+            image: "/images/banner4.jpg",
+            url: "/search?category=combo-shirts",
+            isPublished: true
+        },
+        {
+            title: "Best Seller in T-Shirt",
+            buttonCaption: "Shop Now",
+            image: "/images/banner2.jpg",
+            url: "/search?category=T-Shirts",
+            isPublished: true
+        },
     ]
 }
 
